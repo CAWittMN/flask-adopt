385beabba89d811fc087d1c921a54346e3818760
Casey Wittrock <caseywittrockmn@gmail.com>
2023-05-16 21:07:25 -0500

    add home page route

--- app.py
+++ app.py
@@ -3,13 +3,21 @@ from flask_debugtoolbar import DebugToolbarExtension
 from models import db, connect_db, Pet
 
 app = Flask(__name__)
-
-toolbar = DebugToolbarExtension(app)
-
 app.config["SECRET_KEY"] = "fluffyanimals"
 app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql:///adopt"
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+app.config["SQLALCHEMY_ECHO"] = True
+app.config["SQLALCHEMY_RECORD_QUERIES"] = True
+app.debug = True
+
+debug = DebugToolbarExtension(app)
 
 connect_db(app)
+
 with app.app_context():
     db.create_all()
+
+
+@app.route("/")
+def show_home_page():
+    return render_template("home.html")
