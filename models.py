9a40c73376ab22944d2b400fe018906306ffe4d3
Casey Wittrock <caseywittrockmn@gmail.com>
2023-05-22 11:03:29 -0500

    make logic for leaving input blank if image_url is default

--- models.py
+++ models.py
@@ -2,6 +2,8 @@ from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
+DEFAULT_IMAGE_URL = "https://www.clipartmax.com/png/small/38-387568_labrador-retriever-silhouette-puppy-clip-art-dog-silhouette-transparent-background.png"
+
 
 class Pet(db.Model):
     """Pet model"""
@@ -11,12 +13,18 @@ class Pet(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.Text, nullable=False)
     species = db.Column(db.Text, nullable=False)
-    photo_url = db.Column(db.Text)
+    photo_url = db.Column(db.Text, default=DEFAULT_IMAGE_URL)
     age = db.Column(db.Integer)
     notes = db.Column(db.Text)
     available = db.Column(db.Boolean, nullable=False, default=True)
 
+    @property
+    def return_image_url(self):
+        """Return pet photo url"""
+        if self.photo_url == DEFAULT_IMAGE_URL:
+            return ""
+        return self.photo_url
+
 
 def connect_db(app):
-    db.app = app
     db.init_app(app)
